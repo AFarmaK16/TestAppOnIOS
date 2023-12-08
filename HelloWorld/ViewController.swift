ca34ca7a2179219b780b8b634ee571fc22a7733d
Afarmak16 <kanefarma16@gmail.com>
2023-12-08 12:30:40 +0000

    print message

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by ABC on 07/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+//    @IBAction func showMessage(_ sender: UIButton) {
+//        let alertController = UIAlertController(title: "Hello World", message: "Iyi Gunler:)", preferredStyle: .alert)
+//        alertController.addAction(UIAlertAction(title: "OK", style: .default ,handler: nil))
+//        present(alertController, animated: true, completion: nil)
+//       }
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//                let alertController = UIAlertController(title: "Hello World", message: "Iyi Gunler:)", preferredStyle: .alert)
+//                alertController.addAction(UIAlertAction(title: "OK", style: .default ,handler: nil))
+//        view.backgroundColor = UIColor.brown
+        print("Hello guys")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
