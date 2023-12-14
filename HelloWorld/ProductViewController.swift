ded9a12e5a9e416f3e01a2cbc677e704709b0114
Afarmak16 <kanefarma16@gmail.com>
2023-12-14 16:35:20 +0000

    setup UI for wave Payment

---
+++ HelloWorld/ProductViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ProductViewController.swift
+//  HelloWorld
+//
+//  Created by ABC on 14/12/2023.
+//
+
+import UIKit
+class ProductViewController: UIViewController {
+    
+    
+    //    let wave_Auth_token = "A"
+         
+ //        struct CheckoutRequestBody{
+ //            let amount : String
+ //            let currency =  "XOF"
+ //            let error_url =  "https://example.com/error"
+ //          let success_url = "https://example.com/success"
+ //        }
+ //        struct WaveCheckoutResponse:Codable {
+ //            let id: String
+ //            let amount: String
+ //            let checkout_status: String
+ //            let client_reference : String
+ //            let currency : String
+ //            let error_url : String
+ //            let last_payment_error : String
+ //            let business_name : String
+ //            let payment_status: String
+ //            let success_url: String
+ //            let wave_launch_url: String
+ //            let when_completed: String
+ //            let when_created : String
+ //            let when_expires: String
+ //        }
+    override func viewDidLoad() {
+
+           super.viewDidLoad()
+         //  print("productView ready")
+          self.view.backgroundColor = .systemBlue
+           // Do any additional setup after loading the view.
+       }
+
+}
--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,19 +10,25 @@ import Firebase
 class ViewController: UIViewController {
 
   
+
+  
     override func viewDidLoad() {
-        var ref: DatabaseReference!
-        ref = Database.database().reference()
-        var dataDic: [String: Any] = [:]
-        dataDic["First Name"] = "ABK"
-        dataDic["Last Name"] = "Asym"
-        ref.setValue(dataDic)
-        super.viewDidLoad()
+//        var ref: DatabaseReference!
+//        ref = Database.database().reference()
+//        var dataDic: [String: Any] = [:]
+//        dataDic["First Name"] = "ABK"
+//        dataDic["Last Name"] = "Asym"
+//        ref.setValue(dataDic)
+       super.viewDidLoad()
 
         print("\n\tHello guys\n")
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showProductView(_ sender: Any) {
+        let pc = ProductViewController()
+        self.present(pc, animated: true,completion: nil)
+        //print("button pressed")
+    }
 
 }
 
