ae68e156f9eba4026aae4e607cc90753b46112dd
Afarmak16 <kanefarma16@gmail.com>
2023-12-19 16:04:35 +0000

    created view for wave/om payment

--- HelloWorld/ProductViewController.swift
+++ HelloWorld/ProductViewController.swift
@@ -7,9 +7,59 @@
 
 import UIKit
 class ProductViewController: UIViewController {
-    
+    var price: Int = 0
+    @IBOutlet weak var chossePayView: UIView!
     //    let wave_Auth_token = "A"
          
+    @IBAction func payWithOM(_ sender: UIButton) {
+        print("choosed OM payment")
+    }
+    @IBAction func payWithWave(_ sender: Any) {
+        print("choosed Wave payment")
+                let endpoint = "https://api.wave.com/v1/checkout/sessions"
+        Task { @MainActor in
+
+                           let endpoint = "https://api.wave.com/v1/checkout/sessions"
+                           guard let url = URL(string: endpoint) else {throw MyError.invalidURL}
+                         var request = URLRequest(url: url)
+                         request.httpMethod = "POST"
+                         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                                request.setValue("Bearer wave_sn_prod_KF7S-TZu5rF789Hj3pw_1M0xx28B32olssB-vJi88kBr_P2YA71hnQy6Ta7ZaxMJEy1JWR9WK3oXTewzVbqw8Ysmwr8wlG5QQA", forHTTPHeaderField: "Authorization")
+                         let requestBody: CheckoutRequestBody = CheckoutRequestBody(amount: "\(price)")
+                         do {
+                                    let jsonData = try JSONEncoder().encode(requestBody)
+                                    request.httpBody = jsonData
+                                } catch {
+                                    throw MyError.invalidData
+                                }
+                           let (data,response) = try await URLSession.shared.data(for: request)
+                           print ("Request Response :\(response)")
+                           guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+                               throw MyError.invalidResponse
+                           }
+                           do{
+                               let decoder = JSONDecoder()
+                             //  decoder.keyDecodingStrategy = .convertFromSnakeCase //purhaps won't need it
+                               print("RETRIEVED DATA is ")
+                               print(String(data: data, encoding: .utf8))
+                               let decodedData = try decoder.decode(WaveCheckoutResponse.self, from: data)
+                               print("trying to print data response .... \n \(decodedData)")
+                               if let waveLaunchURL = URL(string: decodedData.wave_launch_url) {
+                                   await UIApplication.shared.open(waveLaunchURL)
+                                           } else {
+                                               print("Invalid wave_launch_url")
+                                           }
+                               return decodedData
+                           } catch let error as DecodingError{
+                               print("invalid data .....",error)
+                               throw MyError.invalidData
+                           }
+                     
+                       }
+    }
+    @IBAction func dismissPayPopup(_ sender: Any) {
+        chossePayView.isHidden = true
+    }
     struct CheckoutRequestBody: Codable{
              let amount : String
         var currency =  "XOF"
@@ -35,53 +85,16 @@ class ProductViewController: UIViewController {
     override func viewDidLoad() {
 
            super.viewDidLoad()
+        chossePayView.isHidden = true
          //  print("productView ready")
        //   self.view.backgroundColor = .systemBlue
            // Do any additional setup after loading the view.
        }
     @IBAction func PerformPayment(_ sender: UIButton) {
-        let price = sender.tag
+        price = sender.tag
         print ("U've selected a product\nWith tag \(price)")
-        let endpoint = "https://api.wave.com/v1/checkout/sessions"
-                Task { @MainActor in
 
-                      let endpoint = "https://api.wave.com/v1/checkout/sessions"
-                      guard let url = URL(string: endpoint) else {throw MyError.invalidURL}
-                    var request = URLRequest(url: url)
-                    request.httpMethod = "POST"
-                    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-                           request.setValue("Bearer wave_sn_prod_KF7S-TZu5rF789Hj3pw_1M0xx28B32olssB-vJi88kBr_P2YA71hnQy6Ta7ZaxMJEy1JWR9WK3oXTewzVbqw8Ysmwr8wlG5QQA", forHTTPHeaderField: "Authorization")
-                    let requestBody: CheckoutRequestBody = CheckoutRequestBody(amount: "\(price)")
-                    do {
-                               let jsonData = try JSONEncoder().encode(requestBody)
-                               request.httpBody = jsonData
-                           } catch {
-                               throw MyError.invalidData
-                           }
-                      let (data,response) = try await URLSession.shared.data(for: request)
-                      print ("Request Response :\(response)")
-                      guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
-                          throw MyError.invalidResponse
-                      }
-                      do{
-                          let decoder = JSONDecoder()
-                        //  decoder.keyDecodingStrategy = .convertFromSnakeCase //purhaps won't need it
-                          print("RETRIEVED DATA is ")
-                          print(String(data: data, encoding: .utf8))
-                          let decodedData = try decoder.decode(WaveCheckoutResponse.self, from: data)
-                          print("trying to print data response .... \n \(decodedData)")
-                          if let waveLaunchURL = URL(string: decodedData.wave_launch_url) {
-                              await UIApplication.shared.open(waveLaunchURL)
-                                      } else {
-                                          print("Invalid wave_launch_url")
-                                      }
-                          return decodedData
-                      } catch let error as DecodingError{
-                          print("invalid data .....",error)
-                          throw MyError.invalidData
-                      }
-                
-                  }
+        chossePayView.isHidden = false
               }
               
 
